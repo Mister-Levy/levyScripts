b7bba1791e2fa1f02f1e11dd54813e075697c2e9
Mister-Levy <damianamodeo@gmail.com>
2022-04-12 20:37:09 +1000

    fix last commit

--- Function Library/theme.lua
+++ Function Library/theme.lua
@@ -281,8 +281,8 @@ function read_rtconfig_to_table(rtconfig_path)
   local rtconfig_table = {}
   local file = io.open(rtconfig_path, "r") --Reading.
   for line in file:lines() do
-        rtconfig_table[#rtconfig_table + 1] = line
-    end
+    rtconfig_table[#rtconfig_table + 1] = line
   end
   file:close()
+  return rtconfig_table
 end
